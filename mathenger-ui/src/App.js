eb45d81b24f35072316644f49362155cb837ff32
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2020-03-25 21:04:25 +0200

    initial react router setup

--- mathenger-ui/src/App.js
+++ mathenger-ui/src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import './App.css';
 import SignInPage from "./components/pages/SignInPage";
+import {BrowserRouter, Redirect} from "react-router-dom";
+import {Route, Switch} from "react-router";
+import {connect} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <SignInPage/>
-    </div>
-  );
+function App({authentication}) {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/signin" component={SignInPage}/>
+                    <Route path="/">
+                        {(authentication.signedIn && <Main/>) || <Redirect to="/signin"/>}
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App;
+function Main() {
+    return (
+        <>
+            <div>Menu</div>
+            <Switch>
+                <Route path="/"><h2>App</h2></Route>
+            </Switch>
+        </>
+    );
+}
+
+const mapStateToProps = state => {
+    return {
+        authentication: state.authentication
+    };
+};
+
+export default connect(mapStateToProps)(App);
