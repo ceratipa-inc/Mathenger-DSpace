377f2e03b2e5d8768abfbcc0e19cc5a0b4a0e4aa
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2020-04-14 22:14:20 +0300

    write simple tests for math to latex compiler

---
+++ mathenger-api/src/main/java/com/example/mathengerapi/services/mathCompiler/MathCompiler.java
@@ -0,0 +1,11 @@
+package com.example.mathengerapi.services.mathCompiler;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MathCompiler {
+    public String toLatex(String mathExpr) {
+        // todo implement
+        return null;
+    }
+}
