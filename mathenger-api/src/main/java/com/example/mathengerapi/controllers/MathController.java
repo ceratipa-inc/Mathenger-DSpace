0b3676f25347290f05fd9c76483af111ef7a2e0b
Roman Boiarchuk <r.s.boyarchuk@gmail.com>
2020-05-14 22:09:06 +0300

    remove old latex converter

--- mathenger-api/src/main/java/com/example/mathengerapi/controllers/MathController.java
+++ mathenger-api/src/main/java/com/example/mathengerapi/controllers/MathController.java
@@ -1,35 +1,18 @@
 package com.example.mathengerapi.controllers;
 
-import com.example.mathengerapi.services.MathService;
 import com.example.mathengerapi.services.mathCompiler.MathCompilerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.util.FileSystemUtils;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.nio.file.Files;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/math")
 @AllArgsConstructor
 public class MathController {
-    private MathService mathService;
     private MathCompilerService mathCompilerService;
-
-    @PostMapping(value = "/latex/transform/png", produces = MediaType.IMAGE_PNG_VALUE)
-    @PreAuthorize("isAuthenticated()")
-    @ResponseBody
-    public ResponseEntity<byte[]> transformToPng(@RequestBody String latex) throws IOException, InterruptedException {
-        var file = mathService.texToPng(latex);
-        byte[] img = Files.readAllBytes(file.toPath());
-        FileSystemUtils.deleteRecursively(file.getParentFile());
-        return new ResponseEntity<>(img, HttpStatus.OK);
-    }
 
     @PostMapping(value = "/transform/latex")
     @PreAuthorize("isAuthenticated()")
